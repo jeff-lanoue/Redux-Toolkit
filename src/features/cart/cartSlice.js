f3cf1077548df5144283f6849e0621f715ee0c72
Jeff Lanoue <jeff.lanoue.1@gmail.com>
2023-02-01 19:21:39 -0800

    Added ability to increase and decrease items

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -21,11 +21,22 @@ const cartSlice = createSlice({
                 (item) => item.id !== itemId
             );
         },
-        // https://www.udemy.com/course/react-tutorial-and-projects-course/learn/lecture/31545368#overview
+        increase: (state, { payload }) => {
+            const cartItem = state.cartItems.find(
+                (item) => item.id === payload.id
+            );
+            cartItem.amount += 1;
+        },
+        decrease: (state, { payload }) => {
+            const cartItem = state.cartItems.find(
+                (item) => item.id === payload.id
+            );
+            cartItem.amount -= 1;
+        },
     },
 });
 
 //console.log(cartSlice);
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
