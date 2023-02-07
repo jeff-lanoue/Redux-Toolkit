d51964010a2af11af819ab083eef848e0a82ab90
Jeff Lanoue <jeff.lanoue.1@gmail.com>
2023-02-06 20:04:36 -0800

    Added confirmation modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Navbar from "./components/Navbar";
-import cartContainer from "./components/CartContainer";
+//import cartContainer from "./components/CartContainer";
 import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./components/Modal";
 
 function App() {
+    const { cartItems } = useSelector((store) => store.cart);
+    const { isOpen } = useSelector((state) => state.modal);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(calculateTotals());
+    }, [cartItems]);
+
     return (
         <main>
+            {isOpen && <Modal />}
             <Navbar />
             <CartContainer />
         </main>
